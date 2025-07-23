7bcc1b594e3cc3f69e0db66568dfefd827c79253
Felipe Novaes Cerqueira <devnovaes@gmail.com>
2025-07-23 16:25:48 +0000

    Adiciona a página `Em construção`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,29 @@
-function Home() {
-  return <h1>Em Construção</h1>;
-}
+import Image from "next/image";
+import styles from "./index.module.css";
 
-export default Home;
+export default function Home() {
+  return (
+    <div className={styles.background}>
+      <Image
+        src="/images/background-construction.png"
+        alt="Plano de fundo"
+        fill
+        priority
+        style={{ objectFit: "cover" }}
+      />
+      <div className={styles.content}>
+        <h1 className={styles.h1}>EM CONSTRUÇÃO</h1>
+        <div className={styles.github}>
+          <a
+            href="https://github.com/DevNovaes/TablesAndGenerators"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.github}
+          >
+            <Image src="/images/github-logo.png" alt="GitHub Logo" fill />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
